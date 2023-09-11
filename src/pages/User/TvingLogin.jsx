066fa763f1a6ce7858c9a4f5466c37b02ea922f9
kimInDa <pinktail2327@naver.com>
2023-09-12 00:19:43 +0900

    [User] init: 티빙로그인 페이지, 티빙회원가입 페이지 컴포넌트 초기 생성 #42
    - 티빙 로그인 페이지와, 티빙 회원가입 페이지를 구현하는 컴포넌트를 초기 생성하였습니다.

---
+++ src/pages/User/TvingLogin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function TvingLogin() {
+  return (
+    <div>
+      TvingLogin
+      <Link to='/user/tvingRegist'>회원가입으로 이동</Link>
+    </div>
+  );
+}
+
+export default TvingLogin;
