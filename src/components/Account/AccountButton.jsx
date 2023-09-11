1e8c2f291b9c88fa4f36e0a7c37aa438d9669527
kimInDa <pinktail2327@naver.com>
2023-09-11 22:05:07 +0900

    [Account] feat: Account 레이아웃과 컴포넌트 병합 및 라우터 설정 #35
    - Account 페이지의 레이아웃과 Button 컴포넌트, Header 컴포넌트를 병합
    - Account 페이지를 리액트 라우터에 추가하였습니다.

--- src/components/Account/AccountButton.jsx
+++ src/components/Account/AccountButton.jsx
@@ -1,27 +1,75 @@
-import AccountImage from "@/assets/Account/account_icons";
-import PropTypes from "prop-types";
-import styles from "./AccountButton.module.css";
+import { NavLink } from 'react-router-dom';
+import AccountImage from '@/assets/Account/account_icons';
+import PropTypes from 'prop-types';
+import styles from './AccountButton.module.css';
 
 const buttons = [
-  { id: 1, text: 'TVING ID로 시작하기', image: AccountImage.tvingIcon},
-  { id: 2, text: '네이버로 시작하기', image: AccountImage.naverIcon},
-  { id: 3, text: '카카오로 시작하기', image: AccountImage.kakaoIcon},
-  { id: 4, text: '페이스북으로 시작하기', image: AccountImage.facebookIcon},
-  { id: 5, text: '트위터로 시작하기', image: AccountImage.twitterIcon},
-  { id: 6, text: 'Apple로 계속하기', image: AccountImage.appleIcon},
-  { id: 7, text: 'CJ ONE으로 시작하기', image: AccountImage.cjIcon},
-
-
+  {
+    id: 1,
+    text: 'TVING ID로 시작하기',
+    image: AccountImage.tvingIcon,
+    link: '/home',
+    isService: true,
+  },
+  {
+    id: 2,
+    text: '네이버로 시작하기',
+    image: AccountImage.naverIcon,
+    link: '#',
+    isService: false,
+  },
+  {
+    id: 3,
+    text: '카카오로 시작하기',
+    image: AccountImage.kakaoIcon,
+    link: '#',
+    isService: true,
+  },
+  {
+    id: 4,
+    text: '페이스북으로 시작하기',
+    image: AccountImage.facebookIcon,
+    link: '#',
+    isService: false,
+  },
+  {
+    id: 5,
+    text: '트위터로 시작하기',
+    image: AccountImage.twitterIcon,
+    link: '#',
+    isService: false,
+  },
+  {
+    id: 6,
+    text: 'Apple로 계속하기',
+    image: AccountImage.appleIcon,
+    link: '#',
+    isService: false,
+  },
+  {
+    id: 7,
+    text: 'CJ ONE으로 시작하기',
+    image: AccountImage.cjIcon,
+    link: '#',
+    isService: false,
+  },
 ];
 
-function AccountButton({ id, text, image }) {
+function AccountButton({ id, text, image, link, isService }) {
   return (
-    <button type="button" className={styles.account__button} key={id}>
-      <div className={styles.button__content}>
-        <img src={image} className={styles.icon__image} alt={text} />
-        <span>{text}</span>
-      </div>
-    </button>
+    <NavLink to={link}>
+      <button type='button' className={styles.account__button} key={id}>
+        <div className={styles.button__content}>
+          <img src={image} className={styles.icon__image} alt={text} />
+        </div>
+        <div className={styles.account__buttonlable__wrpper}>
+          <span>{text}</span>
+          {isService || (
+            <span className={styles.account__onService}>서비스 예정</span>
+          )}
+        </div>
+      </button>
+    </NavLink>
   );
 }
 
@@ -29,7 +77,13 @@ function AccountButtonLIst() {
   return (
     <div>
       {buttons.map((button) => (
-        <AccountButton key={button.id}image={button.image}text={button.text}  />
+        <AccountButton
+          key={button.id}
+          image={button.image}
+          text={button.text}
+          link={button.link}
+          isService={button.isService}
+        />
       ))}
     </div>
   );
@@ -38,7 +92,9 @@ function AccountButtonLIst() {
 export default AccountButtonLIst;
 
 AccountButton.propTypes = {
-  id: PropTypes.number.isRequired,      
-  text: PropTypes.string.isRequired,  
-  image: PropTypes.string.isRequired, 
+  id: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  isService: PropTypes.bool.isRequired,
 };
