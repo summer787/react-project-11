25a16a9ae872937d0bc2be1020a80469f5bc92c7
kimInDa <pinktail2327@naver.com>
2023-09-12 18:23:08 +0900

    [tvingLogin] design: 공통 체크박스 컴포넌트 css 수정 #48
    체크 이미지와 텍스트가 가운데 정렬이 맞지 않는 것을 수정

--- src/pages/User/TvingLogin.jsx
+++ src/pages/User/TvingLogin.jsx
@@ -1,10 +1,13 @@
+import CheckboxRounded from '@/components/User/CheckboxRounded';
+import UserTitle from '@/components/User/UserTitle';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function TvingLogin() {
   return (
     <div>
-      TvingLogin
+      <UserTitle title='TVING ID 로그인' />
+      <CheckboxRounded label='자동로그인' />
       <Link to='/user/tvingRegist'>회원가입으로 이동</Link>
     </div>
   );
