99d16373be27a233b7b8e0c4ca563c86c4a2fd2d
kimInDa <pinktail2327@naver.com>
2023-09-14 21:51:02 +0900

    [tvingLogin]feat: 티빙 로그인 기능 구현 #58
    tiving에 회원가입한 아이디와 비밀번호로 로그인하는 기능 구현

--- src/pages/User/TvingLogin.jsx
+++ src/pages/User/TvingLogin.jsx
@@ -1,9 +1,12 @@
+import React, { useState } from 'react';
+import debounce from '@/utils/debounce';
+import UserTitle from '@/components/User/UserTitle';
 import FormInput from '@/components/Login/FormInput';
 import CheckboxRounded from '@/components/User/CheckboxRounded';
 import UserInfo from '@/components/User/UserInfo';
-import UserTitle from '@/components/User/UserTitle';
-import debounce from '@/utils/debounce';
-import React, { useState } from 'react';
+import pb from '@/api/pocketbase';
+import { useNavigate } from 'react-router-dom';
+import Spinner from '@/components/Spinner';
 
 const userInfo = {
   text: '아직 계정이 없으신가요?',
@@ -17,17 +20,41 @@ function TvingLogin() {
     id: '',
     password: '',
   });
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleInput = debounce((e) => {
     const { name, value } = e.target;
     setFormState({ ...formState, [name]: value });
   });
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { id, password } = formState;
+
+    try {
+      setIsLoading(true);
+      const response = await pb
+        .collection('users')
+        .authWithPassword(id, password);
+      console.log(response);
+      setIsLoading(false);
+      navigate('/home');
+    } catch (error) {
+      setIsLoading(false);
+      const errorMessage =
+        '일치하는 회원정보가 없습니다.\n이용하시는 계정 유형(TVING ID/CJ ONE/SNS)과\n아이디, 비밀번호를 다시 확인해주세요.';
+      // eslint-disable-next-line no-alert
+      alert(errorMessage);
+    }
+  };
+
   return (
     <div>
       <UserTitle title='TVING ID 로그인' />
 
-      <form>
+      <form onSubmit={handleLogin}>
         <FormInput
           type='text'
           name='id'
@@ -44,9 +71,9 @@ function TvingLogin() {
         />
         <button type='submit'>로그인하기</button>
       </form>
-
       <CheckboxRounded label='자동로그인' />
       <UserInfo userInfo={userInfo} />
+      <Spinner isOpen={isLoading} />
     </div>
   );
 }
