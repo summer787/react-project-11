9eb7ef567bdae74776eb74515e9871692b150ee5
kimInDa <pinktail2327@naver.com>
2023-09-14 13:36:42 +0900

    [tvingLogin] feat: 로그인 기능 구현-FormInput컴포넌트 생성 #53
    로그인 시 아이디와 비밀번호를 입력하는 FormInput 컴포넌트를 생성하였습니다.

--- src/pages/User/TvingLogin.jsx
+++ src/pages/User/TvingLogin.jsx
@@ -1,7 +1,9 @@
+import FormInput from '@/components/Login/FormInput';
 import CheckboxRounded from '@/components/User/CheckboxRounded';
 import UserInfo from '@/components/User/UserInfo';
 import UserTitle from '@/components/User/UserTitle';
-import React from 'react';
+import debounce from '@/utils/debounce';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const userInfo = {
@@ -12,9 +14,38 @@ const userInfo = {
 };
 
 function TvingLogin() {
+  const [formState, setFormState] = useState({
+    id: '',
+    password: '',
+  });
+
+  const handleInput = debounce((e) => {
+    const { name, value } = e.target;
+    setFormState({ ...formState, [name]: value });
+  });
+
   return (
     <div>
       <UserTitle title='TVING ID 로그인' />
+
+      <form>
+        <FormInput
+          type='text'
+          name='id'
+          label='아이디'
+          autoComplete='username'
+          onChange={handleInput}
+        />
+        <FormInput
+          type='password'
+          name='password'
+          label='비밀번호'
+          autoComplete='current-password'
+          onChange={handleInput}
+        />
+        <button type='submit'>로그인하기</button>
+      </form>
+
       <CheckboxRounded label='자동로그인' />
       <Link to='/user/tvingRegist'>
         <UserInfo userInfo={userInfo} />
