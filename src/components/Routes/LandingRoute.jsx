f066ed6604571607ddead7f1fe83483e129e4bd3
kimInDa <pinktail2327@naver.com>
2023-09-19 18:15:16 +0900

    [tvingLogin] fix: 로그인 유무에 따라 다른 랜딩 페이지 구현 #83

--- src/components/Routes/LandingRoute.jsx
+++ src/components/Routes/LandingRoute.jsx
@@ -1,5 +1,3 @@
-import Home from '@/pages/Home';
-import Account from '@/pages/Account/Account';
 import useStorage from '@/hooks/useStorage';
 import { useAuth } from '@/components/contexts/AuthContext';
 import { Navigate } from 'react-router-dom';
@@ -9,9 +7,9 @@ function LandingRoute() {
   const { storageData } = useStorage('pocketbase_auth');
 
   return isAuth || storageData ? (
-    <Navigate to='/home' replace={true} />
+    <Navigate to='/home' replace />
   ) : (
-    <Navigate to='/account' replace={true} />
+    <Navigate to='/account' replace />
   );
 }
 
