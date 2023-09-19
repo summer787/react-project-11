8457c906189a19e76b68a8e8abf73f9086160636
kimInDa <pinktail2327@naver.com>
2023-09-19 16:37:17 +0900

    [tvingLogin] feat: 로그인 유무에 따라 다른 랜딩 페이지 구현 #83
    로그인 유무에 따라 다른 랜딩 페이지 구현

--- src/layout/MainHeader/MainHeader.jsx
+++ src/layout/MainHeader/MainHeader.jsx
@@ -1,17 +1,20 @@
 // /src/layout/MainHeader/MainHeader.jsx
-import MainHeaderImage from "@/assets/MainHeader/MainHeader_image";
-import Search from "@/components/Search/Search";
-import Nav from "@/components/Nav/Nav";
-import { Link } from "react-router-dom";
-import styles from "./MainHeader.module.css";
+import MainHeaderImage from '@/assets/MainHeader/MainHeader_image';
+import Search from '@/components/Search/Search';
+import Nav from '@/components/Nav/Nav';
+import { Link } from 'react-router-dom';
+import { useAuth } from '@/components/contexts/AuthContext';
+import styles from './MainHeader.module.css';
 
 function MainHeader() {
+  const { isAuth } = useAuth();
+
   return (
     <div>
       <header className={styles.header}>
         <h1>
-          <Link to="/">
-            <img src={MainHeaderImage.logo} alt="티빙" />
+          <Link to={isAuth ? '/home' : '/'}>
+            <img src={MainHeaderImage.logo} alt='티빙' />
           </Link>
         </h1>
         <div className={styles.navigation}>
--- src/layout/UserHeader/UserHeader.jsx
+++ src/layout/UserHeader/UserHeader.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useAuth } from '@/components/contexts/AuthContext';
 import styles from './UserHeader.module.css';
 
 function UserHeader() {
+  const { isAuth } = useAuth();
+
   return (
     <header className={styles.account__header}>
       <h1 className='a11yHidden'>티빙 로고</h1>
-      <Link to='/'>
+      <Link to={isAuth ? '/home' : '/'}>
         <img
           src='/account_tving_logo.svg'
           className={styles.header__img}
