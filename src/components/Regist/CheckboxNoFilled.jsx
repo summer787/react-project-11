7fc79a9664be64e71e89821d35c8c993c7ce3d4d
kimInDa <pinktail2327@naver.com>
2023-09-19 15:10:22 +0900

    [tivingRegist] feat: 회원가입 페이지 checkbox리스트 상태구현 #78
    회원가입 페이지 checkbox 리스트 상태 구현

--- src/components/Regist/CheckboxNoFilled.jsx
+++ src/components/Regist/CheckboxNoFilled.jsx
@@ -1,35 +1,32 @@
 import { string } from 'prop-types';
-import { useState} from 'react';
+import { useState } from 'react';
 import style from './CheckboxNoFilled.module.css';
 // 아이디 라벨이랑 연결하기기
-function CheckboxNoFilled({ label }) {
-  const [isChecked, setIsChecked] = useState(false);
+function CheckboxNoFilled({ checked, label, onChange }) {
+  // const [isChecked, setIsChecked] = useState(false);
 
-  const handleCheck = () => {
-    setIsChecked(!isChecked);
-  };
+  // const handleCheck = () => {
+  //   setIsChecked(!isChecked);
+  //   onChange();
+  // };
 
   return (
     <div className={style.nofilled__checkbox__wrapper}>
       <input
         type='checkbox'
         name='nofilled__checkbox'
-        id=  {label}
+        id={label}
         className={style.nofilled__checkbox}
-        onChange={handleCheck}
+        onChange={onChange}
       />
       <label
         htmlFor={label}
-
-
         className={`${style.nofilled__checkbox__label} ${
-          isChecked && style.checked
+          checked && style.checked
         }`}
-        onChange={handleCheck}
+        // onChange={handleCheck}
       >
         {label}
-
-        
       </label>
     </div>
   );
