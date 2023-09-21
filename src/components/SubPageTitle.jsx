24218d6d5fce30256c0d35f824109b4cb1724302
kangsuhyeon <sagomia@naver.com>
2023-09-21 14:33:39 +0900

    Feat : 버튼기능 구현 #99 - 찜 버튼의 기능을 구현합니다     - 찜 버튼을 누르면 '포스트가 찜되었습니다'라는 팝업 창이 표시되도록 기능을 구현합니다. - 공유 버튼 기능 구현     - 공유 버튼의 기능을 구현합니다.     - 공유 버튼을 클릭하면 '카카오톡, 트위터, 페이스북, 링크 복사' 옵션을 제공하는 모달 대화 상자를 구현합니다.     - '링크 복사' 버튼을 누르면 현재 링크가 복사되도록 기능을 구현합니다.     - '링크 복사' 버튼을 클릭하면 '콘텐츠의 주소가 복사되었습니다'라는 팝업 창이 표시되도록 기능을 구현합니다. - 플레이 버튼 기능 구현     - 플레이 버튼의 기능을 구현합니다.     - VideoPlayer 영역에서 섬네일 이미지를 클릭했을 경우, 플레이 버튼을 클릭한 섬네일 이미지로 이동할 수 있도록 기능을 구현합니다.

--- src/components/SubPageTitle.jsx
+++ src/components/SubPageTitle.jsx
@@ -5,22 +5,52 @@ import getImageURL from "@/utils/getImageURL";
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
+import playIcon from '../assets/Subpage/triangle_icon.png';
 import likeIcon from '../assets/Subpage/like_icon.svg';
+import likedIcon from '../assets/Subpage/liked_icon.svg';
 import shareIcon from '../assets/Subpage/share_icon.svg';
+import facebook from '../assets/Subpage/facebook_icon.svg';
+import kakaotalk from '../assets/Subpage/kakaotalk_icon.svg';
+import twitter from '../assets/Subpage/twitter_icon.svg';
+import link from '../assets/Subpage/link_icon.svg';
 import sub from "../styles/subpage.module.css";
 
 function SubPageTitle (){
     const {id} = useParams()
     const [data, setData] = useState()
     const [showSharePopup, setShowSharePopup] = useState(false);
-    const copyToClipboard = async (text) => {
-        try {
-          await navigator.clipboard.writeText(text);
-          alert("링크가 클립보드에 복사되었습니다.");
-        } catch (err) {
-          alert("클립보드에 복사하지 못했습니다.", err);
-        }
-      };
+    const [isExpanded, setIsExpanded] = useState(false);
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    }
+    const [showLinkCopiedPopup, setShowLinkCopiedPopup] = useState(false);
+    const handleLinkClick = () => {
+        // 현재 URL 복사
+        navigator.clipboard.writeText(window.location.href);
+        // 링크 복사 알림 팝업창 보이기
+        setShowLinkCopiedPopup(true);
+        // 일정 시간 후에 팝업창 숨기기
+        setTimeout(() => {
+            setShowLinkCopiedPopup(false);
+        }, 2000);  // 예: 2초 후에 숨김
+    };
+    const [isLiked, setIsLiked] = useState(false); 
+    const [showPostPopup, setShowPostPopup] = useState(false);
+    const handleLikeIconClick = () => {
+        setIsLiked(!isLiked); 
+   };
+   const handleShowPostPopup = () => { 
+    setShowPostPopup(true);
+    setTimeout(() => {
+        setShowPostPopup(false);
+    }, 2000);
+    }
+    const handleClick = () => { 
+        handleLikeIconClick(); 
+        handleShowPostPopup(); 
+    }; 
+
+
     useEffect(()=>{
         async function getTv(){
             try {
@@ -35,20 +65,21 @@ function SubPageTitle (){
     },[id])
 //    id값이 변경될 때 마다 useEffect 안에 있는 것들이 실행됨
 // console.log(data);
+    
 if(data) {
     const {title, audiencerating, release, broadcasting, season, creator, actor, description, expand} = data;
     const {tag: tagArray} = expand;
     const {tag}=tagArray[0];
-    
+    console.log(data)
     return(
         <>
         <Helmet>
-            <title>소용없어 거짓말</title>
+            <title>{title}</title>
         </Helmet>
         {/* 헬멧으로 title을 바꿔준다 */}
         <div>
-            <section className={sub.contentInformation}>
-                <div className={sub.backgroundImage}/>
+            <section className={`${sub.contentInformation} ${isExpanded ? sub.PageTitleExpanded : ''}`}>
+                <div className={sub.backgroundImage} style={{ backgroundImage: `url(${getImageURL(data, 'poster')})` }}/>               
                 <div className={sub.blurOverlay}/>
                 <div className={sub.content}>
                         <article className={sub.contentMain}>
@@ -67,32 +98,41 @@ if(data) {
                             </ul>
                             <nav className={sub.Buttons}>
                                 <button type='button' className={sub.playButton}>
-                                    시즌 1: Episode 시청하기
+                                    <img className={sub.playIcon} src={playIcon} alt="triangleIcon"/>
+                                    <p>시즌 1: Episode 시청하기</p>
                                 </button>
-                                <button type='button' className={sub.likeButton}>
-                                    <img className={sub.likeIcon} src={likeIcon} alt="Like" />
+                                <button type='button' className={sub.likeButton} onClick={handleClick}>
+                                    <img className={sub.likeIcon}  src={isLiked ? likedIcon : likeIcon} alt="Like" />
                                     <span>찜</span>
                                 </button>
+                                {showPostPopup && (
+                                <div className={sub.postPopup}>
+                                    포스트가 찜되었습니다.
+                                </div>
+                                )}
+                            <div className={sub.shareWrapper}>
                                 <button type='button' className={sub.shareButton} onClick={() => setShowSharePopup(true)} >
                                 <img className={sub.shareIcon} src={shareIcon} alt="Share" />
                                     <span>공유</span>
                                 </button>
                                 <div 
-                                className={sub.sharePopupWrapper} 
+                                className={`${sub.sharePopupWrapper} ${showSharePopup ? sub.visible : ''}`} 
                                 onClick={() => setShowSharePopup(false)} // 바깥 부분 클릭 시 팝업 닫기
                                 >
                                     {showSharePopup && (
                                         <div className={sub.sharePopup}
                                         onClick={(e) => e.stopPropagation()} 
                                         >
-                                            <input type="text" value={`https://www.yourwebsite.com/video/${id}`} readOnly />
-                                            <button 
-                                            onClick={() => copyToClipboard(`https://www.yourwebsite.com/video/${id}`)} 
-                                            >
-                                            </button>
-                                            <button onClick={() => setShowSharePopup(false)}>닫기</button>
+                                            <button type="button"><img src={facebook} alt="Facebook Share" /></button>
+                                            <button type="button"><img src={twitter} alt="Twitter Share" /></button>
+                                            <button type="button"><img src={kakaotalk} alt="Kakao Share" /></button>
+                                            <button type="button"  onClick={handleLinkClick}><img src={link} alt="Link Share" /></button>
+                                            {showLinkCopiedPopup && (
+                                                <div className={sub.linkCopiedPopup}>콘텐츠의 주소가 복사되었습니다.</div>
+                                            )}
                                         </div>
                                     )}
+                                </div>
                                 </div>
                             </nav>
                             <div className={sub.information}>
@@ -100,13 +140,13 @@ if(data) {
                                     <dt>크리에이터</dt>
                                     <dd>{creator}</dd>
                                 </dl>
-                                <dl className={sub.actor}>
+                                <dl className={isExpanded ? sub.actorClamp : sub.actor}>
                                     <dt>출연</dt>
                                     <dd>{actor}</dd>
                                 </dl>
-                                {/* 더보기 버튼 누르면 확장 & 생략 되는 거 기능 구현 */}
+                                <p className={`${sub.description} ${isExpanded ? '' : sub.descriptionClamp}`}>{description}</p>
+                                <button onClick={toggleExpand} type="button">{isExpanded ? "닫기" : "더보기" }</button>
                             </div>
-                            <p className={sub.description}>{description}</p>
                         </article>
                         <article className={sub.posterWrap}> 
                             <img className={sub.poster} src={getImageURL(data, 'poster')}  alt='오랫동안 당신을 기다렸습니다' />
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -27,6 +27,7 @@ function VideoPlayer(){
             try {
                 const record = await pb.collection('tv').getOne(id, {expand: 'tag'}, {expand: 'seasonDescription'});
                 setData(record);
+                console.log()
                 
                 // Check if the record exists and has the necessary properties
                 if (record && record.seasonDescription && record.seasonRelease) {
@@ -50,144 +51,142 @@ function VideoPlayer(){
 
 
 if(data) {
-    console.log(data)
+    const  {title} =data;
     return(
-       
-
         <section className={sub.videoPlayer}>
-        <article className={sub.videoPlayerHeader}>
-            {/* <Select 
-                className="select__control css-1s2u09g-control"
-                classNamePrefix="select"
-                options={options}
-                isSearchable={false}
-                placeholder="소용없어 거짓말(총 12화)"
-            /> */}
-            <div className={sub.videoPlayerTitle}>
-                <h2 className={sub.title}>
-                    소용없어 거짓말
-                </h2>
-                <h3 className={sub.number}>
-                    (총 12화)
-                </h3>
-                <svg fill='#fff' width="40" height="40" viewBox="0 0 32 32"/>
-            </div>
-            <div className={sub.orderChoices}>
-                <div className={sub.clickOn}>
-                    <button type="button" className={selectedButton === "first" ? sub.clickFirst : sub.clickOn}
-                    onClick={() => {
-                        setEpisodeIndices([...episodeIndices].sort((a,b) => a - b));
-                        setSelectedButton("first");
-                    }}
-                    >첫화부터</button>
-                    <button type="button" className={`${selectedButton === "new" ? sub.clickNewBtn:  sub.clickOn} ${sub.clickNew} ${sub.buttonWithEffects}`}
-                    onClick={() => {
-                        setEpisodeIndices([...episodeIndices].sort((a,b) => b - a));
-                        setSelectedButton("new");
-                    }}>최신화부터</button>
+            <article className={sub.videoPlayerHeader}>
+                {/* <Select 
+                    className="select__control css-1s2u09g-control"
+                    classNamePrefix="select"
+                    options={options}
+                    isSearchable={false}
+                    placeholder="소용없어 거짓말(총 12화)"
+                /> */}
+                <div className={sub.videoPlayerTitle}>
+                    <h2 className={sub.title}>
+                        {title}
+                    </h2>
+                    <h3 className={sub.number}>
+                        (총 12화)
+                    </h3>
+                    <svg fill='#fff' width="40" height="40" viewBox="0 0 32 32"/>
                 </div>
-                <label className={sub.switch} htmlFor="s1">
-                    <span className={sub.sliderRound}> 연속재생</span>
-                    <input type="checkbox" id="s1"/>
-                </label>
-            </div>
-        </article>
-        <article className={sub.videoPlayerInformation}>
-        <Swiper
-            modules={[ Navigation, Scrollbar]}
-            breakpoints={{
-                
-                768: {
-                    slidesPerView: 2,},
-                929:{
-                    slidesPerView: 2.2,},
-                1024: {
-                    slidesPerView: 2.5,},
-                1300:{
-                    slidesPerView: 3,},
-                1500:{
-                    slidesPerView: 3.8,
-                },
-                1920:{
-                    slidesPerView : 4.8 ,}
-            }}
-            navigation={{
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'}}
-            scrollbar={{ 
-                el: '.swiper-scrollbar',
-                draggable: true ,
-                dragSize: 500 
-            }}
-            >    
-            {data &&
-            data.thumbnailSeason1 &&
-            episodeIndices.map((index) => (
-            <SwiperSlide key={index}>
-                <div className={selectedEpisode === index ? sub.seasonThumbnailWrap1 : sub.seasonThumbnailWrap }
-                onClick={()=>setSelectedEpisode(index)} 
-                onKeyDown={(event) => {
-                    if (event.key === 'Enter' || event.key === ' ') {
-                        event.preventDefault();
-                        setSelectedEpisode(index);
-                    }
-                }}
-                tabIndex={0} 
-                >
-                    <img
-                        className={sub.seasonThumbnail}
-                        src={getImageURLThumbnail(data,  index, "thumbnailSeason1")}
-                        alt="thumbnail"
-                    /> 
-                <button
-                    type="button"
-                    className={selectedEpisode === index ? sub.thumbnailPlayButton : null}
-                    aria-label="Play" 
-                >
-                    {selectedEpisode === index && (
-                        <svg
-                        className={sub.svgIcon}
-                        data-name="Layer 1"
-                        id="Layer_1"
-                        viewBox="0 0 100 100"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fill="#ffffff"
-                            d="M50,5A45,45,0,1,0,95,50,45.00058,45.00058,0,0,0,50,5Zm0,88A43.00024,43.00024,0,1,1,80.40552,80.40552,42.86137,42.86137,0,0,1,50,93Z"
-                        />
-                        <path
-                            fill="#ffffff"
-                            d="M69.78345,47.76355,40.96753,29.40466a2.54663,2.54663,0,0,0-1.37195-.4046,2.5884,2.5884,0,0,0-1.81012.74756A2.64065,2.64065,0,0,0,37,31.64111V68.35889a2.64062,2.64062,0,0,0,.78546,1.89343,2.588,2.588,0,0,0,1.81012.74756,2.54663,2.54663,0,0,0,1.37195-.4046L69.78345,52.23645a2.66383,2.66383,0,0,0,0-4.4729Zm-1.07477,2.78619L39.89282,68.90857a.54048.54048,0,0,1-.29724.09131.591.591,0,0,1-.40777-.17353A.634.634,0,0,1,39,68.35889V31.64111a.63378.63378,0,0,1,.18781-.46746.59111.59111,0,0,1,.40777-.17359.54161.54161,0,0,1,.29724.09131L68.70874,49.45032a.66419.66419,0,0,1-.00006,1.09942Z"
-                        />
-
-                    </svg>
-                    )}
-                </button>
-                    
-                    <div className={sub.itemInformation}>
-                        <h2 className={sub.itemTitle}>{`${[index+1]}. 소용없어 거짓말 ${index + 1}화`}</h2> 
-                        {data && descriptionArray && (
-                            <p className={sub.itemDescription}>
-                            {descriptionArray[index]}
-                            </p>
-                        )}
-                        {data&& releaseArray &&(
-                            <p className={sub.itemSubInformation}>{releaseArray[index]}</p>
-                        )}
+                <div className={sub.orderChoices}>
+                    <div className={sub.clickOn}>
+                        <button type="button" className={selectedButton === "first" ? sub.clickFirst : sub.clickOn}
+                        onClick={() => {
+                            setEpisodeIndices([...episodeIndices].sort((a,b) => a - b));
+                            setSelectedButton("first");
+                        }}
+                        >첫화부터</button>
+                        <button type="button" className={`${selectedButton === "new" ? sub.clickNewBtn:  sub.clickOn} ${sub.clickNew} ${sub.buttonWithEffects}`}
+                        onClick={() => {
+                            setEpisodeIndices([...episodeIndices].sort((a,b) => b - a));
+                            setSelectedButton("new");
+                        }}>최신화부터</button>
                     </div>
+                    <label className={sub.switch} htmlFor="s1">
+                        <span className={sub.sliderRound}> 연속재생</span>
+                        <input type="checkbox" id="s1"/>
+                    </label>
                 </div>
-              </SwiperSlide>
-            )
-            )
-            
-            }
+            </article>
+            <article className={sub.videoPlayerInformation}>
+            <Swiper
+                modules={[ Navigation, Scrollbar]}
+                breakpoints={{
+                    
+                    768: {
+                        slidesPerView: 2,},
+                    929:{
+                        slidesPerView: 2.2,},
+                    1024: {
+                        slidesPerView: 2.5,},
+                    1300:{
+                        slidesPerView: 3,},
+                    1500:{
+                        slidesPerView: 3.8,
+                    },
+                    1920:{
+                        slidesPerView : 4.8 ,}
+                }}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'}}
+                scrollbar={{ 
+                    el: '.swiper-scrollbar',
+                    draggable: true ,
+                    dragSize: 500 
+                }}
+                >    
+                {data &&
+                data.thumbnailSeason1 &&
+                episodeIndices.map((index) => (
+                <SwiperSlide key={index}>
+                    <div className={selectedEpisode === index ? sub.seasonThumbnailWrap1 : sub.seasonThumbnailWrap }
+                    onClick={()=>setSelectedEpisode(index)} 
+                    onKeyDown={(event) => {
+                        if (event.key === 'Enter' || event.key === ' ') {
+                            event.preventDefault();
+                            setSelectedEpisode(index);
+                        }
+                    }}
+                    tabIndex={0} 
+                    >
+                        <img
+                            className={sub.seasonThumbnail}
+                            src={getImageURLThumbnail(data,  index, "thumbnailSeason1")}
+                            alt="thumbnail"
+                        /> 
+                    <button
+                        type="button"
+                        className={selectedEpisode === index ? sub.thumbnailPlayButton : null}
+                        aria-label="Play" 
+                    >
+                        {selectedEpisode === index && (
+                            <svg
+                            className={sub.svgIcon}
+                            data-name="Layer 1"
+                            id="Layer_1"
+                            viewBox="0 0 100 100"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill="#ffffff"
+                                d="M50,5A45,45,0,1,0,95,50,45.00058,45.00058,0,0,0,50,5Zm0,88A43.00024,43.00024,0,1,1,80.40552,80.40552,42.86137,42.86137,0,0,1,50,93Z"
+                            />
+                            <path
+                                fill="#ffffff"
+                                d="M69.78345,47.76355,40.96753,29.40466a2.54663,2.54663,0,0,0-1.37195-.4046,2.5884,2.5884,0,0,0-1.81012.74756A2.64065,2.64065,0,0,0,37,31.64111V68.35889a2.64062,2.64062,0,0,0,.78546,1.89343,2.588,2.588,0,0,0,1.81012.74756,2.54663,2.54663,0,0,0,1.37195-.4046L69.78345,52.23645a2.66383,2.66383,0,0,0,0-4.4729Zm-1.07477,2.78619L39.89282,68.90857a.54048.54048,0,0,1-.29724.09131.591.591,0,0,1-.40777-.17353A.634.634,0,0,1,39,68.35889V31.64111a.63378.63378,0,0,1,.18781-.46746.59111.59111,0,0,1,.40777-.17359.54161.54161,0,0,1,.29724.09131L68.70874,49.45032a.66419.66419,0,0,1-.00006,1.09942Z"
+                            />
+
+                        </svg>
+                        )}
+                    </button>
+                        
+                        <div className={sub.itemInformation}>
+                            <h2 className={sub.itemTitle}>{`${[index+1]}. ${title} ${index + 1}화`}</h2> 
+                            {data && descriptionArray && (
+                                <p className={sub.itemDescription}>
+                                {descriptionArray[index]}
+                                </p>
+                            )}
+                            {data&& releaseArray &&(
+                                <p className={sub.itemSubInformation}>{releaseArray[index]}</p>
+                            )}
+                        </div>
+                    </div>
+                </SwiperSlide>
+                )
+                )
                 
-                </Swiper>
-                <div className="swiper-button-next" style={{ position: "absolute",width: "3.888rem", top: "50%", right: "10px", color:"white", padding:"5px",zIndex: 9999}}/>
-                <div className="swiper-button-prev" style={{ position: "absolute",width: "3.888rem", top: "50%", left: "10px",  color:"white", padding:"5px", zIndex: 9999}}/>
-                <div className="swiper-scrollbar" style={{ position: "absolute",  right: "0", top: "300px", bottom: "0", backgroundColor: '#404040'}}/>
-        </article>
+                }
+                    
+                    </Swiper>
+                    <div className="swiper-button-next" style={{ position: "absolute",width: "3.888rem", top: "50%", right: "10px", color:"white", padding:"5px",zIndex: 9999}}/>
+                    <div className="swiper-button-prev" style={{ position: "absolute",width: "3.888rem", top: "50%", left: "10px",  color:"white", padding:"5px", zIndex: 9999}}/>
+                    <div className="swiper-scrollbar" style={{ position: "absolute",  right: "0", top: "300px", bottom: "0", backgroundColor: '#404040'}}/>
+            </article>
     </section>  
     )}
     
