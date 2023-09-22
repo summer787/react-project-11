871958bafda49a07bceddcef4311f01cf74c82b9
kimInDa <pinktail2327@naver.com>
2023-09-23 02:41:30 +0900

    [Common] 웹 접근성 개선 #94
    alt와 콘텐츠 텍스트 분리

--- src/components/Account/AccountButton.jsx
+++ src/components/Account/AccountButton.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from 'react-router-dom';
-import AccountImage from '@/assets/Account/account_icons';
 import { bool, number, string } from 'prop-types';
+import AccountImage from '@/assets/Account/account_icons';
 import styles from './AccountButton.module.css';
 
 const buttons = [
   {
     id: 1,
     text: 'TVING ID로 시작하기',
+    alt: '티빙 아이콘',
     image: AccountImage.tvingIcon,
     link: '/user/tvingLogin',
     isService: true,
@@ -14,6 +15,7 @@ const buttons = [
   {
     id: 2,
     text: '네이버로 시작하기',
+    alt: '네이버 아이콘',
     image: AccountImage.naverIcon,
     link: '#',
     isService: false,
@@ -21,6 +23,7 @@ const buttons = [
   {
     id: 3,
     text: '카카오로 시작하기',
+    alt: '카카오 아이콘',
     image: AccountImage.kakaoIcon,
     link: '#',
     isService: false,
@@ -28,6 +31,7 @@ const buttons = [
   {
     id: 4,
     text: '페이스북으로 시작하기',
+    alt: '페이스북 아이콘',
     image: AccountImage.facebookIcon,
     link: '#',
     isService: false,
@@ -35,6 +39,7 @@ const buttons = [
   {
     id: 5,
     text: '트위터로 시작하기',
+    alt: '트위터 아이콘',
     image: AccountImage.twitterIcon,
     link: '#',
     isService: false,
@@ -42,6 +47,7 @@ const buttons = [
   {
     id: 6,
     text: 'Apple로 계속하기',
+    alt: '애플 아이콘',
     image: AccountImage.appleIcon,
     link: '#',
     isService: false,
@@ -49,18 +55,19 @@ const buttons = [
   {
     id: 7,
     text: 'CJ ONE으로 시작하기',
+    alt: '씨제이원 아이콘',
     image: AccountImage.cjIcon,
     link: '#',
     isService: false,
   },
 ];
 
-export function AccountButton({ id, text, image, link, isService }) {
+export function AccountButton({ id, text, alt, image, link, isService }) {
   return (
     <NavLink to={link} key={id}>
       <button type='button' className={styles.account__button}>
         <div className={styles.button__content}>
-          <img src={image} className={styles.icon__image} alt={text} />
+          <img src={image} className={styles.icon__image} alt={alt} />
         </div>
         <div className={styles.account__buttonlable__wrpper}>
           <span>{text}</span>
@@ -91,12 +98,14 @@ function AccountButtonLIst() {
 }
 
 AccountButton.defaultProps = {
+  alt: '',
   isService: true,
 };
 
 AccountButton.propTypes = {
   id: number.isRequired,
   text: string.isRequired,
+  alt: string,
   image: string.isRequired,
   link: string.isRequired,
   isService: bool,
