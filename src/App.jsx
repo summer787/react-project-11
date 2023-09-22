5e689da3a14d2854728f2ded41c96568028dfdd1
kimInDa <pinktail2327@naver.com>
2023-09-22 15:38:50 +0900

    [tvingLogin] User 관련 페이지들에 Helmet 추가 #101
    Helmet 설치 및 provider 입력

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 // /src/App.jsx
-import { RouterProvider } from 'react-router-dom';
 import { Suspense } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import router from './routes';
 import AuthProvider from './components/contexts/AuthContext';
 import FindUserProvider from './components/contexts/FindUserContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <FindUserProvider>
-        <Suspense fallback={<div>Loading</div>}>
-          <RouterProvider router={router} />
-        </Suspense>
-      </FindUserProvider>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <FindUserProvider>
+          <Suspense fallback={<div>Loading</div>}>
+            <RouterProvider router={router} />
+          </Suspense>
+        </FindUserProvider>
+      </AuthProvider>
+    </HelmetProvider>
   );
 }
 
