817c3e6eaaa83b2a5a3334f2ac199c61b0efc5fe
leesy <leesy787@gmail.com>
2023-09-23 03:09:12 +0900

    [RegistComplete] feat : 회원가입 완료 페이지 애니메이션 추가#109

--- src/pages/User/RegistComplete.jsx
+++ src/pages/User/RegistComplete.jsx
@@ -1,5 +1,6 @@
 import CheckedIcon from "@/assets/Regist/check_mark_circle_icon.svg";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 import UserButton from "@/components/User/UserButton";
 import styles from "@/pages/User/RegistComplete.module.css";
 
@@ -12,8 +13,26 @@ function RegistComplete() {
 
   return (
     <div className={styles.complete__container}>
-      <img src={CheckedIcon} alt="checkicon" className={styles.check__img} />
-      <h2 className={styles.complete__title}>회원가입 완료</h2>
+      <motion.img
+        initial={{ scale: 0 }}
+        animate={{ rotate: 360, scale: 1.1 }}
+        transition={{
+          type: "spring",
+          stiffness: 300,
+          damping: 40,
+        }}
+        src={CheckedIcon}
+        alt="checkicon"
+        className={styles.check__img}
+      />
+      <motion.h2
+        className={styles.complete__title}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.05, duration: 0.5 }}
+      >
+        회원가입 완료
+      </motion.h2>
       <h2 className={styles.complete__title}>
         티빙 회원이 되신 것을 환영해요!
       </h2>
@@ -25,7 +44,9 @@ function RegistComplete() {
       >
         최신 인기 TV프로그램, 영화를 무제한으로 만나보세요!
       </p>
-      <UserButton onClick={handleClick} text="로그인 하기" isActive />
+      <div className={styles.button__container}>
+        <UserButton onClick={handleClick} text="로그인 하기" isActive />
+      </div>
     </div>
   );
 }
